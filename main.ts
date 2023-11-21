d2166c153eef7c8c22267537524b38b855cc6e42
Ashu-kd <a_dubey@ee.iitr.ac.in>
2023-11-21 20:46:02 +0530

    added block for multiplication

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    multiply()
+})
+function multiply () {
+    n = n * 2
+}
+let n = 0
 basic.showIcon(IconNames.Heart)
+n = 1
 basic.forever(function () {
-    basic.showString("Hello!")
+    basic.showString("" + (n))
 })
